66ad856748b75faf3b1e32e049eeb5f2af7083dc
manoshiro <shirodai@gmail.com>
2012-08-17 16:32:50 +0900

    Adding active method in model of movie.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,15 +1,12 @@
-require '/usr/local/apps/movie_seen/app/models/movies_reslut'
-
 class Movie < ActiveRecord::Base
+  scope :active, lambda {
+    where :negative => 0
+  }
+
   include ApplicationModel
 
   has_many :seens
   has_many :affiliates
-
-  MOVIESRESUT_KIND_OF_SEENS  = 245
-  MOVIESRESUT_KIND_OF_STARS  = 246
-  MOVIESRESUT_KIND_OF_WISHES = 248
-
 
   def self.text_search(word, matches = Movie)
     word.split(/[[:space:]]/, 3).each do |sentence|
@@ -29,32 +26,6 @@ class Movie < ActiveRecord::Base
   def category_tag
     tags = self.category.split ','
     Tag.where(:id => tags, :attrib => 0, :negative => 0)
-  end
-
-  def result_of_seen
-    result MOVIESRESUT_KIND_OF_SEENS
-  end
-
-  def result_of_star
-    result MOVIESRESUT_KIND_OF_STARS
-  end
-
-  def result_of_wish
-    result MOVIESRESUT_KIND_OF_WISHES
-  end
-
-  def result(kind_id)
-    movie_result = MoviesResult.where(
-      :movie_id => self.id, :kind_id  => kind_id, :negative => 0
-    ).first
-    if movie_result.blank?
-      movie_result = MoviesResult.new
-      movie_result.movie_id = self.id
-      movie_result.kind_id  = kind_id
-      movie_result.number   = false
-      movie_result.negative = 0
-    end
-    movie_result
   end
 
   def seen_users(author_id = nil)
