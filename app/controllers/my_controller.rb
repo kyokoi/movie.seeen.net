1a0d44060c7402c371ddd8f84a51aec74e477d35
manoshiro <shirodai@gmail.com>
2012-08-17 23:49:44 +0900

    Adding recommend user on summary page.

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -3,6 +3,7 @@
 class MyController < ApplicationController
 
   SUMMARY_LIMIT = 3
+  RECOMMEND_DIR = '/usr/local/apps/movie_seen/data/recommend'
 
   def activity
     @my = Author.active.where(:id => params[:author_id]).first
@@ -29,8 +30,28 @@ class MyController < ApplicationController
     @wish_seen = @wish_seen.order('date desc').order('id desc')
     @wish_seen = @wish_seen.limit(SUMMARY_LIMIT)
 
+    @recommend_users = fetch_recommend @my.id
+
     @display        = 3
     @weekly_ranking = fetch_ranking [:weekly_movie]
     @wishs_ranking  = fetch_ranking [:wishs]
+  end
+
+
+  private
+
+  def fetch_recommend(author_id)
+    directory_name = "#{RECOMMEND_DIR}/#{Digest::MD5.hexdigest(author_id.to_s)[0, 2]}"
+    users = YAML.load_file "#{directory_name}/#{author_id}.yml"
+    recommends = users.map do |user_id, point|
+      recommend_user = Author.active.where(:id => author_id)
+      if recommend_user.first.blank?
+        next
+      end
+      {:id => user_id, :name => recommend_user.first.name}
+    end
+    recommends
+  rescue Exception => e
+    []
   end
 end
