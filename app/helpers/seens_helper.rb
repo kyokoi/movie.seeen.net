237efa8b2911566b87035d3111e5e34fe2d96520
manoshiro <shirodai@gmail.com>
2012-08-30 23:45:05 +0900

    伏せ画像　名前対応

--- app/helpers/seens_helper.rb
+++ app/helpers/seens_helper.rb
@@ -1,2 +1,16 @@
+# encoding: utf-8
+
 module SeensHelper
+  def author_name_for_unlogin_user(star, wish, recently)
+    if star && star.count > 0
+      return "「#{star.first.movie.name_of_japan}」がお気に入りさん"
+    end
+    if wish && wish.count > 0
+      return "「#{wish.first.movie.name_of_japan}」が見たい人さん"
+    end
+    if recently && recently.count > 0
+      return "「#{recently.first.movie.name_of_japan}」を見た人さん"
+    end
+    "もっと映画をみましょうさん"
+  end
 end
