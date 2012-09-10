ce3308965bc7e3f1fa4f3ae4de563127a504ba67
manoshiro <shirodai@gmail.com>
2012-09-10 12:18:27 +0900

    title change.

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -2,7 +2,7 @@
 
 class AboutController < ApplicationController
   def index
-    page_title "映画箱：観た映画管理サイト　観たい映画管理サイト"
+    page_title "観た映画管理サイト　観たい映画管理サイト"
     description "観た映画は記録しよう。観たい映画もメモしよう。思い出の映画は、管理、記録、メモ。自分だけの映画履歴を作っちゃおう。日本最大級の映画管理サイトを目指します。"
   end
 
