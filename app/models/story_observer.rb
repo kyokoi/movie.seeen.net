382dc539e9c1bdb17535b20078afc346c44480bb
manoshiro <shirodai@gmail.com>
2012-09-16 12:58:06 +0900

    削除された映画IDの関連を関連テーブルから削除。

--- app/models/story_observer.rb
+++ app/models/story_observer.rb
@@ -1,16 +1,25 @@
 class StoryObserver < ActiveRecord::Observer
-   def after_save(story)
-     story.search_movie do |line, mark, movie|
+  def after_save(story)
+    related_movies = []
+    story.search_movie do |line, mark, movie|
+      related_movies << movie
       unless story.movies.detect{|registed_movie| registed_movie == movie}
         story.movies << movie
       end
     end
-   end
+
+    removed_movies = story.movies.reject do |movie|
+      related_movies.map{|movie|movie.id}.include? movie.id
+    end
+
+    removed_movies.each do |movie|
+      story.movies.delete movie
+    end
+  end
 
   def after_destroy(story)
     story.movies.each do |movie|
       movie.destroy
     end
   end
- end
-
+end
