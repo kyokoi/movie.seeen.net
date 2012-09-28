511d841a118cb585dbdd4e61ee26d28ccebe97a3
manoshiro <shirodai@gmail.com>
2012-09-28 22:54:22 +0900

    return

--- tool/summary/execute.rb
+++ tool/summary/execute.rb
@@ -26,9 +26,23 @@ monitor.append 'users.twitter', authors.where(:provider => 'twitter').count
 monitor.append 'users.google_oauth2', authors.where(:provider => 'google_oauth2').count
 
 # seens
-seens = Seen.where :negative => 0
+seens = Seen.active.where(Seen.no_wish)
 monitor.append 'seens.all', seens.count
 
+# added authors
+added_authors = Author.where("created_at BETWEEN ? AND ?","#{Date.yesterday} 00:00:00", "#{Date.today} 23:59:59'")
+monitor.append 'yesterday.added.users', added_authors.count
 
+# added movies
+added_movies = Movie.where("created_at BETWEEN ? AND ?","#{Date.yesterday} 00:00:00", "#{Date.today} 23:59:59'")
+monitor.append 'yesterday.added.movies', added_movies.count
+
+# added seens
+added_seens = Seen.all_seens.where("created_at BETWEEN ? AND ?","#{Date.yesterday} 00:00:00", "#{Date.today} 23:59:59'")
+monitor.append 'yesterday.added.seens', added_seens.count
+
+# added wishlist
+added_wishlist = Seen.wishlist.where("created_at BETWEEN ? AND ?","#{Date.yesterday} 00:00:00", "#{Date.today} 23:59:59'")
+monitor.append 'yesterday.added.wishlist', added_wishlist.count
 monitor.aging
 
